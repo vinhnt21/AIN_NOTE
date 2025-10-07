e84f94f7a2619106580a5ad212e6461b549ed88a
vinhnt21 <vinhnek117@gmail.com>
2025-10-07 13:32:25 +0700

    Enhance script.js to dynamically set document title based on URL file name, improving user experience and clarity.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 // change favicon
 document.addEventListener("DOMContentLoaded", function () {
   try {
+
+
+    // query file name from url path and change title document
+    const fileName = window.location.pathname.split("/").pop();
+    document.title = fileName;
+    console.log("File name:", fileName.split(".")[0]);
+    // change title document
+    document.title = fileName.split(".")[0].toUpperCase();
+    console.log("File name:", fileName.split(".")[0]);
+
     // Add Vietnamese fonts
     const fontLinks = [
       "https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
